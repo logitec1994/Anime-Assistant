9e5e738f98adc50a1a389a8c6342a9c4754db478
logitec1994 <123654978a@gmail.com>
2025-08-07 11:46:37 +0300

    new

--- main.py
+++ main.py
@@ -1,28 +1,2 @@
-import asyncio
-import logging
-import sys
-from os import getenv
-
-from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from bot.handlers.start import router as start_router
-
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TOKEN = getenv("BOT_TOKEN")
-
-dp = Dispatcher()
-dp.include_router(start_router)
-
-async def main() -> None:
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    await dp.start_polling(bot)
-
-
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
+    print("Hello, World!")
