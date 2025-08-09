f29f3153850067da850b0f55fa21098914b7c8ad
logitec1994 <123654978a@gmail.com>
2025-08-09 15:22:56 +0300

    fix: Add try-except block to handle IntegrityError in add_item service method

--- app/services/item_service.py
+++ app/services/item_service.py
@@ -2,20 +2,24 @@ from sqlalchemy.orm import Session
 from database.repositories import ItemRepository
 from shared.dto import ItemCreateDTO
 
+from sqlalchemy.exc import IntegrityError
+
 class ItemService:
     def __init__(self, db_session: Session):
         self.repository = ItemRepository(db_session)
     
-    def add_item(self, item: ItemCreateDTO) -> ItemCreateDTO:
-        new_item = self.repository.add_item(item)
-        self.repository.db_session.commit()
-        self.repository.db_session.refresh(new_item)
-        returned_item = ItemCreateDTO(
-            # Need to use new_item instead of getattr
-            id=getattr(new_item, "id"), # To avoid type incompatibility warning
-            title=getattr(new_item, "title"), # To avoid type incompatibility warning
-            status=getattr(new_item, "status"), # To avoid type incompatibility warning
-            category=getattr(new_item, "category") # To avoid type incompatibility warning
-        )
-
-        return returned_item
+    def add_item(self, item: ItemCreateDTO) -> ItemCreateDTO | str:
+        try:
+            new_item = self.repository.add_item(item)
+            self.repository.db_session.commit()
+            self.repository.db_session.refresh(new_item)
+            return ItemCreateDTO(
+                # Need to use new_item instead of getattr
+                id=getattr(new_item, "id"), # To avoid type incompatibility warning
+                title=getattr(new_item, "title"), # To avoid type incompatibility warning
+                status=getattr(new_item, "status"), # To avoid type incompatibility warning
+                category=getattr(new_item, "category") # To avoid type incompatibility warning
+            )
+        except IntegrityError:
+            self.repository.db_session.rollback()
+            return "Item already exists with this title"
