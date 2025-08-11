a9e05cb15db6b1d2b6f9700a0b1151881934b725
logitec1994 <123654978a@gmail.com>
2025-08-11 19:50:43 +0300

    Refactor bot startup and import structure

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
-from app.services.item_service import ItemService
-from database.session import SessionLocal
-from shared.dto import ItemCreateDTO, ItemUpdateDTO
-from shared.enums import ItemStatus, ItemCategory
+import asyncio
+from bot.bot import main
 
 if __name__ == "__main__":
-    # title = "Naruto"
-    with SessionLocal() as db_session:
-        service = ItemService(db_session)
-        item_list = service.get_items()
-        print(item_list)
-
-        print("\nBefore update")
-
-        update_item = ItemUpdateDTO(id=2, status=ItemStatus.WATCHED)
-        result = service.update_status(update_item)
-        print(result)
-
-        print("After update\n")
-
-        item_list = service.get_items()
-        print(item_list)
-
-    print(item_list)
+    asyncio.run(main=main())
